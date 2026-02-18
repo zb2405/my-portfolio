add881681ff10baee6a91000e8766abbc7839179
Zaki Bawade <zb2405@rit.edu>
2026-02-18 18:49:30 +0000

    changed about

--- zaki-portfolio-v11/app/about/page.tsx
+++ zaki-portfolio-v11/app/about/page.tsx
@@ -2,14 +2,9 @@
 import React from 'react';
 import { AboutSection } from '../../components/about/AboutSection';
 
-export default function AboutPage() {
+export function AboutPage() {
   return (
     <main className="relative w-full min-h-screen">
-      {/*
-        Global Layout Wrapper:
-        - pl-[96px]: 72px sidebar + 24px gap
-        - pt-28: Standard top padding for pages
-      */}
       <div className="pl-[96px] pt-28">
         <AboutSection />
       </div>
